babc87e4338a650184e63f448be50f2ade61edbf
wwnss00 <wns208500@gmail.com>
2025-02-13 23:48:04 +0900

    중첩클래스

---
+++ chpa03/src/sec01/exam03/A.java
@@ -0,0 +1,19 @@
+package sec01.exam03;
+
+public class A {
+    int field1;
+    void method1(){}
+
+    static int field2;
+    static void method2(){}
+
+    static class C {
+        void mehtod() {
+            field1 = 10;
+            method1();
+
+            field2 = 10;
+            method2();
+        }
+    }
+}
