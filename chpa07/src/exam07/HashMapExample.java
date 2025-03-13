a9cec57f645812f444683e7744e7cb8ee47b3d75
wwnss00 <wns208500@gmail.com>
2025-03-13 23:37:45 +0900

    컬렉션 프레임워크

---
+++ chpa07/src/exam07/HashMapExample.java
@@ -0,0 +1,15 @@
+package exam07;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        Map<Student,Integer> map = new HashMap<>();
+
+        map.put(new Student(1,"홍길동"), 95);
+        map.put(new Student(1,"홍길동"), 95);
+
+        System.out.println("총 Entry 수: " + map.size());
+    }
+}
