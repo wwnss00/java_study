8f774df0c9ebda09f8074d20f7371c8ce6708312
wwnss00 <wns208500@gmail.com>
2025-06-09 00:07:51 +0900

    compare

---
+++ java-mid2/src/collection/compare/SortMain1.java
@@ -0,0 +1,13 @@
+package collection.compare;
+
+import java.util.Arrays;
+
+public class SortMain1 {
+    public static void main(String[] args) {
+        Integer[] array = {3,2,1};
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("기본 정렬 후");
+        System.out.println(Arrays.toString(array));
+    }
+}
